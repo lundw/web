1a095294de1aff65a4539166523c3f94c72fbd98
Will Lund <lundw@cwu.edu>
2014-10-11 18:15:28 -0700

    Created index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+	<head>
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"/>
+		<!-- Optional style-->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css"/>
+		<!-- Custom CS-->
+		<link type="text/css" rel="stylesheet" href="main.css"/>
+		<!-- Latest compiled and minified CSS -->
+		<title>Learning Index</title>
+		<?php
+		include "Header_Linkbar.php";
+		?>
+	</head>
+	<body>
+		<div class="container">
+		<div class="row">
+			<div class="#ffff00 col-sm-6">test</div>
+			<div class="#000000 col-sm-6">test1</div>
+			<div class="#123456 col-md-8">Bigger scrren test</div>
+			<div class="col-md-4">Bigger screen test1</div>
+			<div class="col-xs-3 col-sm-6 col-md-8 col-lg-4">Scaling screen test</div>
+			<div class="col-xs-9 col-sm-6 col-md-4 col-lg-8">Scaling screen test1</div>
+		</div>
+		</div>
+		<!-- Jquery -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	</body>
+	<footer>
+	<?php
+	echo("Hello, I am footer");
+	?> 
+	</footer>
+</html>
