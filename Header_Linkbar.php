9a2bc35f32f22d7668f74dbbd5a05cf465ccad88
Will Lund <lundw@cwu.edu>
2014-10-14 00:11:19 -0700

    Updated the Header_Linkbar to include real links

--- Header_Linkbar.php
+++ Header_Linkbar.php
@@ -17,28 +17,25 @@
 				<li class="active"><a href="#">Link</a></li>
 				<li><a href="#">Link</a></li>
 				<li class="dropdown">
-				  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+				  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Information <span class="caret"></span></a>
 				  <ul class="dropdown-menu" role="menu">
-					<li><a href="#">Action</a></li>
-					<li><a href="#">Another action</a></li>
-					<li><a href="#">Something else here</a></li>
+					<li><a href="#Contact_Us.php">Contact Us</a></li>
+					<li><a href="#About_Us.php">About Us</a></li>
 					<li class="divider"></li>
-					<li><a href="#">Separated link</a></li>
-					<li class="divider"></li>
-					<li><a href="#">One more separated link</a></li>
+					<li><a href="#">About the Website</a></li>
 				  </ul>
 				</li>
 			  </ul>
 			  <form class="navbar-form navbar-left" role="search">
 				<div class="form-group">
-				  <input type="text" class="form-control" placeholder="Search">
+				  <input type="text" class="form-control" placeholder="Enter search text here">
 				</div>
-				<button type="submit" class="btn btn-default">Submit</button>
+				<button type="submit" class="btn btn-default">Search</button>
 			  </form>
 			  <ul class="nav navbar-nav navbar-right">
 				<li><a href="#">Link</a></li>
 				<li class="dropdown">
-				  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+				  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Log In <span class="caret"></span></a>
 				  <ul class="dropdown-menu" role="menu">
 					<li><a href="#">Action</a></li>
 					<li><a href="#">Another action</a></li>
