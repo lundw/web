e9e94781e1265afd9b4ce6f0b7f2605c23c663cf
Will Lund <lundw@cwu.edu>
2014-10-22 11:44:08 -0700

    Added AJAX page loading

--- index.php
+++ index.php
@@ -14,16 +14,20 @@
 	</head>
 	<body>
 		<div class="container">
-		<div class="row">
-			<div class="#ffff00 col-sm-6">test</div>
-			<div class="#000000 col-sm-6">test1</div>
-			<div class="#123456 col-md-8">Bigger scrren test</div>
-			<div class="col-md-4">Bigger screen test1</div>
-			<div class="col-xs-3 col-sm-6 col-md-8 col-lg-4">Scaling screen test</div>
-			<div class="col-xs-9 col-sm-6 col-md-4 col-lg-8">Scaling screen test1</div>
+		<div class="row" id="main">
+			<?php  
+			include "pageDisplay.php";
+			?>
+
+
+
 		</div>
 		</div>
 		<!-- Jquery -->
+		<script type="text/javascript">
+		$('#main').empty();
+		$('#main').load('<?php echo $page?>');
+		</script>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
